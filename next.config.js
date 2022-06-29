08ac416fbc8e050af9a8f8d0babb9ed34dc1a201
ZiXyos <killian.moudery@gmail.com>
2022-06-29 14:37:12 +0200

    chore(update): version + add env for cdn

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	 experimental: {
+	swcMinify: true,
+	experimental: {
     	outputStandalone: true,
   	},
+	serverRuntimeConfig: {
+        API_URL: process.env.API_URL,
+    },
+	publicRuntimeConfig: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    }
 }
 
 module.exports = nextConfig
